0afb26f580fce6543f2e7f0955cf44772ac497d1
Ahmad Maghrobi <achmedmy@gmail.com>
2025-12-02 22:00:27 +0700

    menambahkan data diri

---
+++ latihan-2/index-test-2.js
@@ -0,0 +1,9 @@
+let name = "Ahmad"
+let age = 33
+let hasJob = true
+let city = "Surabaya"
+let job = "technical director"
+
+console.log("Hello! My name is " + name + ".");
+console.log("I'm " + age + " years old.");
+console.log("I'm a " + job + " from " + city + ".");
